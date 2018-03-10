4bd5e586929edc1a17fa6fd9eb21d01b3cdca534
auzundere <admin@192.168.1.2>
2018-03-10 13:41:30 -0600

    Abdullah add hello

--- src/Main/GoodEvening.java
+++ src/Main/GoodEvening.java
@@ -4,7 +4,7 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		
 		System.out.println("Good Evening Class!");
-		
+		System.out.println("Hello to Selami from Abdullah");
 		for(int i=0; i<12; i++) {
 			System.out.println("It is still early to go home");
 		}
