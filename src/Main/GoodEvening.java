0f7116ef93d3ce7bda9f9a647330eaf078e964cf
selami3448 <selami@192.168.1.7>
2018-03-10 10:45:52 -0600

    initial commit for my second git project

--- src/Main/GoodEvening.java
+++ src/Main/GoodEvening.java
@@ -1,5 +1,10 @@
 package Main;
 
 public class GoodEvening {
+	public static void main(String[] args) {
+		
+		System.out.println("Good Evening Class!");
+		
+	}
 
 }
--- src/Main/GoodMorning.java
+++ src/Main/GoodMorning.java
@@ -2,4 +2,10 @@ package Main;
 
 public class GoodMorning {
 
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Good Morning!");
+		
+	}
 }
